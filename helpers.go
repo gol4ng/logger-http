378fc8dd2b0ca82cf93c5c82797a91ce80d6ac57
Anthony Moutte <instabledesign@gmail.com>
2019-12-13 17:42:32 +0100

    fix test unit

--- helpers.go
+++ helpers.go
@@ -8,7 +8,7 @@ import (
 func MessageWithFileLine(message string, skip int) string {
 	_, file, line, ok := runtime.Caller(1 + skip)
 	if ok {
-		message += " a" + file + ":" + strconv.Itoa(line)
+		message += " " + file + ":" + strconv.Itoa(line)
 	}
 	return message
 }
